556f9814b138656f3540d72da68ac3f2bd6076ce
vidyak9 <srividya.kummara@fiserv.com>
2022-11-10 16:48:29 +0530

    Added tests,added lombok gotchas

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package guru.springwork.spring5recipeapp;
+package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
